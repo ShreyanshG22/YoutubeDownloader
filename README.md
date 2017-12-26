a4da29dba685da47b7d1e9b36c1bdba9b4a9f38f
ShreyanshG22 <gopawarsr15.it@coep.ac.in>
2017-12-26 23:04:47 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+By Default
+	Download as audio and first result in search
+	
+-v		Download as video
+-c		Download whole channel
+-l		List of 10 search result to choose from
+-f <filename>	Take names from file
+-fv <filename>	Take names from file and download video
+
+* YSong.py is basically a python program to download audio/video from youtube.
+* It uses selenium for web scraping.
+* youtube-dl must be installed for this program to work.
+* USAGE :
+	python YSong.py <options>
---
+++ YSong.py
@@ -0,0 +1,108 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import os
+import sys
+
+def download(w):
+	command_tokens = [
+	    'youtube-dl',
+	    '--extract-audio',
+	    '--audio-format mp3',
+	    '--audio-quality 0',
+	    '--output \'%(title)s.%(ext)s\'',
+	    w]
+
+	command = ' '.join(command_tokens)
+	os.system(command)
+
+def downloadv(w):
+	command_tokens = [
+	    'youtube-dl',
+	    '-f best',
+	    '-ciw',
+	    '--output \'%(title)s.%(ext)s\'',
+	    w]
+
+	command = ' '.join(command_tokens)
+	os.system(command)
+
+
+def get_url(driver, a):
+	url = 'https://www.youtube.com/results?search_query=' + a
+	driver.get(url)
+	w = WebDriverWait(driver, 20).until(lambda driver : driver.find_element_by_xpath('//*[@id="video-title"]').get_attribute("href"))
+	return w
+
+def get_channel_url(driver, a):
+	url = 'https://www.youtube.com/results?search_query=' + a
+	driver.get(url)
+	w = WebDriverWait(driver, 20).until(lambda driver : driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[3]/ytd-item-section-renderer/div[2]/ytd-channel-renderer/a').get_attribute("href"))
+	return w
+
+def getlist(driver, a) :
+	url = 'https://www.youtube.com/results?search_query=' + a
+	driver.get(url)
+	
+	SongList = []
+	for i in range(1, 11):
+		s = "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[3]/ytd-item-section-renderer/div[2]/ytd-video-renderer[" + str(i) + "]/div[1]/div/div[1]/div/h3/a"
+		w = WebDriverWait(driver, 20).until(lambda driver: driver.find_element_by_xpath(s).get_attribute("href"))
+		SongName = WebDriverWait(driver, 20).until(lambda driver: driver.find_element_by_xpath(s).get_attribute("title"))
+		print "[" + str(i) + "] " + SongName
+		SongList.append(w)
+	k = raw_input('Selection --> ')
+	url = SongList[int(k)-1]
+	return url
+
+def main():
+	driver = webdriver.Firefox()
+	
+	if '-f' in sys.argv[1:]:
+		with open(sys.argv[2], 'r') as f:
+                	songs = f.readlines()
+                	print(songs)
+                	songs = map(lambda s: s.strip().split(" "), songs)
+                	songs = map(lambda s:"+".join(s), songs)
+                	for i in songs:
+                		url = get_url(driver, i)
+                		download(url)
+
+	elif '-fv' in sys.argv[1:]:
+		with open(sys.argv[2], 'r') as f:
+                	songs = f.readlines()
+                	print(songs)
+                	songs = map(lambda s: s.strip().split(" "), songs)
+                	songs = map(lambda s:"+".join(s), songs)
+                	for i in songs:
+                		url = get_url(driver, i)
+                		downloadv(url)
+
+	else:
+		if '-c' in sys.argv[1:]:
+			a = raw_input('Channel Name --> ').strip().split(" ")
+			a = "+".join(a)
+			url = get_channel_url(driver, a)
+			print(url)
+			
+		elif '-l' in sys.argv[1:]:
+			a = raw_input('Song/Video Name --> ').strip().split(" ")
+			a = "+".join(a)
+			url = getlist(driver, a)
+			print(url)
+		
+		else:
+			a = raw_input('Song/Video Name --> ').strip().split(" ")
+			a = "+".join(a)
+			url = get_url(driver, a)
+			print(url)
+		
+		if '-v' in sys.argv[1:]:
+			downloadv(url)
+		
+		else:
+			download(url)
+		
+	driver.close()
+	
+if __name__ == '__main__':
+    main()
