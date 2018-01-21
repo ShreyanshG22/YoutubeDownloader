4f25f811cd94745d83227e9ddea1267330b3ca93
ShreyanshG22 <gopawarsr15.it@coep.ac.in>
2018-01-21 14:08:40 +0530

    Added playlist option

--- YSong.py
+++ YSong.py
@@ -25,6 +25,14 @@ def downloadv(w):
 
 	command = ' '.join(command_tokens)
 	os.system(command)
+	
+def downloadp(w):
+	command_tokens = [
+		'youtube-dl',
+		'-cit',
+		w]
+	command = ' '.join(command_tokens)
+	os.system(command)
 
 
 def get_url(driver, a):
@@ -38,6 +46,37 @@ def get_channel_url(driver, a):
 	driver.get(url)
 	w = WebDriverWait(driver, 20).until(lambda driver : driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[3]/ytd-item-section-renderer/div[2]/ytd-channel-renderer/a').get_attribute("href"))
 	return w
+
+def get_playlist_url(driver, a):
+	url = 'https://www.youtube.com/results?search_query=' + a
+	driver.get(url)
+	w = WebDriverWait(driver, 20).until(lambda driver : driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[3]/ytd-item-section-renderer/div[2]/ytd-playlist-renderer[1]/div/a').get_attribute("href"))
+	w = w.split('=')
+	print(w[2])
+	p = 'https://www.youtube.com/playlist?list=' + w[2]
+	return p
+
+def get_play_url(driver, a) :
+	url = 'https://www.youtube.com/results?search_query=' + a
+	driver.get(url)
+	
+	SongList = []
+	try:
+		for i in range(1, 6):
+			s = "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[3]/ytd-item-section-renderer/div[2]/ytd-playlist-renderer["+ str(i) +"]/div/a"
+			w = WebDriverWait(driver, 20).until(lambda driver: driver.find_element_by_xpath(s).get_attribute("href"))
+			w = w.split('=')
+			p = 'https://www.youtube.com/playlist?list=' + w[2]
+			s = "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[3]/ytd-item-section-renderer/div[2]/ytd-playlist-renderer["+ str(i) +"]/div/a/h3/span"
+			SongName = WebDriverWait(driver, 20).until(lambda driver: driver.find_element_by_xpath(s).get_attribute("title"))
+			print "[" + str(i) + "] " + SongName
+			SongList.append(p)
+		k = raw_input('Selection --> ')
+		url = SongList[int(k)-1]
+		return url
+	except:
+		pass
+
 
 def getlist(driver, a) :
 	url = 'https://www.youtube.com/results?search_query=' + a
@@ -84,6 +123,18 @@ def main():
 			url = get_channel_url(driver, a)
 			print(url)
 			
+		elif '-p' in sys.argv[1:]:
+			a = raw_input('Playlist Name --> ').strip().split(" ")
+			a = "+".join(a)
+			url = get_playlist_url(driver, a)
+			print(url)
+		
+		elif '-pl' in sys.argv[1:]:
+			a = raw_input('Playlist Name --> ').strip().split(" ")
+			a = "+".join(a)
+			url = get_play_url(driver, a)
+			print(url)
+			
 		elif '-l' in sys.argv[1:]:
 			a = raw_input('Song/Video Name --> ').strip().split(" ")
 			a = "+".join(a)
@@ -98,7 +149,8 @@ def main():
 		
 		if '-v' in sys.argv[1:]:
 			downloadv(url)
-		
+		elif '-p' or '-pl' in sys.argv[1:]:
+			downloadp(url)
 		else:
 			download(url)
 		
